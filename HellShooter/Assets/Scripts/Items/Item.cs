4abc6aef82af9bd9c68489840884da74be7001d3
lewlol <lewdevbusiness@gmail.com>
2025-03-29 08:05:46 +0000

    Project Removal

--- HellShooter/Assets/Scripts/Items/Item.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Item/Item")]
-public class Item : ScriptableObject
-{
-    [Header("Item Information")]
-    public string itemName;
-    public GameObject itemPrefab;
-}
